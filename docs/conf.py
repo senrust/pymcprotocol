85c8ba6f52bf7cc60ce58e1fafdb0184a9515ad7
Yohei Osawa <yohei.osawa@open-mc.com>
2020-11-26 00:49:31 +0900

    Succeeded to make API reference

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,11 @@
 #
 import os
 import sys
+
+
+sys.path.insert(0, os.path.abspath('../src'))
 sys.path.insert(0, os.path.abspath('../src/pymcprotocol'))
+
 
 # -- Project information -----------------------------------------------------
 
@@ -32,7 +36,7 @@ release = version
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon'
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
