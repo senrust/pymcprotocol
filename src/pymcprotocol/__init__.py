5c10caf4aa04cf0d811aa42f1aba71a4737413b7
Yohei Osawa <yohei.osawa.318.niko8@gmail.com>
2021-01-21 10:26:32 +0900

    Fixed version

--- src/pymcprotocol/__init__.py
+++ src/pymcprotocol/__init__.py
@@ -1,8 +1,8 @@
-__version__      = '0.2.3'
+__version__      = '0.2.4'
 __license__      = 'MIT'
 __author__       = 'Yohei Osawa'
 __author_email__ = 'yohei.osawa.318.niko8@gmail.com'
-__url__          = 'https://github.com/yohei250r/pymcprotocol'
+__url__          = 'https://github.com/senrust/pymcprotocol'
 
 from .type3e import Type3E
 from .type4e import Type4E
